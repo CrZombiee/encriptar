8dd81cae78e20190a25d0b17f0efa7f9b65f66ed
CrZombie <dctriana42@misena.edu.co>
2024-02-20 20:46:00 -0500

    terminar archivo para territorio

---
+++ Extencion.php
@@ -0,0 +1,23 @@
+<?php 
+/*
+$array = array(
+    "a", 
+    "b"
+);
+
+$remplazo = array(
+    0 => "dif"
+);
+
+$final = array_replace(
+    $array,$remplazo
+);
+var_dump($final);
+*/
+
+$array= array(
+    "hola" => "adios"
+);
+
+var_dump($array);
+?>

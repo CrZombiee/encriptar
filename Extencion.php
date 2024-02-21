87749248639bff4a787725ee276fd24b009604eb
CrZombie <dctriana42@misena.edu.co>
2024-02-20 20:58:38 -0500

    actualizacion

--- Extencion.php
+++ Extencion.php
@@ -15,6 +15,55 @@ $final = array_replace(
 var_dump($final);
 */
 
+/*
+$datos = "";
+if (isset($_POST['dato'])) {
+    $datos = $_POST['dato'];
+}
+if (!empty($datos))
+{
+    // llena el arreglo, caracter a caracter, y dimensiona el arreglo, 
+    //según la cantidad de caracteres, de la cadena de caracteres
+    $datos = str_split($_POST['dato']); 
+    echo "<pre>";
+    print_r($datos);
+    echo "</pre>";
+    // muestra el contenido del arreglo con for
+    for ($i=0; $i<count($datos);$i++)
+    {
+        echo($datos[$i]);
+        echo "<br>";
+    }
+} 
+else 
+{
+    echo "No
+Tú
+20:35
+$datos = "";
+if (isset($_POST['dato'])) {
+    $datos = $_POST['dato'];
+}
+if (!empty($datos))
+{
+    // llena el arreglo, caracter a caracter, y dimensiona el arreglo, 
+    //según la cantidad de caracteres, de la cadena de caracteres
+    $datos = str_split($_POST['dato']); 
+    echo "<pre>";
+    print_r($datos);
+    echo "</pre>";
+    // muestra el contenido del arreglo con for
+    for ($i=0; $i<count($datos);$i++)
+    {
+        echo($datos[$i]);
+        echo "<br>";
+    }
+} 
+else 
+{
+    echo "No
+*/
+
 $array= array(
     "hola" => "adios"
 );
